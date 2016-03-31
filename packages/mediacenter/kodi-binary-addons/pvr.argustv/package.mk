cf8e8a25890b4d08646a5ac782810eb3ac8fc3b4
Lukas Rusak <lorusak@gmail.com>
2016-03-30 23:15:56 -0700

    kodi-binary-addons: update to latest versions

--- packages/mediacenter/kodi-binary-addons/pvr.argustv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.argustv/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.argustv"
-PKG_VERSION="654715b"
+PKG_VERSION="45a64b7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
