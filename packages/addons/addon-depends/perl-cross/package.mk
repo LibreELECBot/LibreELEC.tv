3928b7b79bd72c28068c8f906f7991c3da45b66e
jc <awiouy@gmail.com>
2016-05-26 06:51:20 +0200

    perl: create addon

---
+++ packages/addons/addon-depends/perl-cross/package.mk
@@ -0,0 +1,46 @@
+
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="perl-cross"
+PKG_VERSION="1.0.2" 
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://arsv.github.io/perl-cross/"
+PKG_URL="https://github.com/arsv/perl-cross/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="tools"
+PKG_PRIORITY="optional"
+PKG_SHORTDESC="perl-cross"
+PKG_LONGDESC="Cross-compiling perl"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+configure_target() {
+  :
+}
+
+make_target() {
+  :
+}
+
+makeinstall_target() {
+  :
+}
