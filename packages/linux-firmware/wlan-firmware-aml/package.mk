7b6e4b314cde8086f1fb6ffe3a08fd36046508c8
Stanislav Vlasic <svlasic@gmail.com>
2016-09-16 19:35:12 +0200

    [wlan-firmware-aml] Copy more firmwares and nvram files which DHD driver will automatically load

--- packages/linux-firmware/wlan-firmware-aml/package.mk
+++ packages/linux-firmware/wlan-firmware-aml/package.mk
@@ -38,7 +38,31 @@ make_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/lib/firmware/brcm
+  # AP6181
+  cp -PR bcm_ampak/config/AP6181/Wi-Fi/fw_bcm40181a2*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/AP6181/Wi-Fi/nvram_ap6181.txt $INSTALL/lib/firmware/brcm/ap6181_nvram.txt
+  # AP6210
+  cp -PR bcm_ampak/config/AP6210/Wi-Fi/fw_bcm40181a2*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/AP6210/Wi-Fi/nvram_ap6210.txt $INSTALL/lib/firmware/brcm/ap6210_nvram.txt
+  cp -P bcm_ampak/config/AP6210/BT/bcm20710a1.hcd $INSTALL/lib/firmware/brcm
+  # AP6212
+  cp -PR bcm_ampak/config/6212/fw_bcm43438a0*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/6212/nvram.txt $INSTALL/lib/firmware/brcm/ap6212_nvram.txt
+  cp -P bcm_ampak/config/6212/BT/bcm43438a0.hcd $INSTALL/lib/firmware/brcm
+  # AP6330
+  cp -PR bcm_ampak/config/AP6330/Wi-Fi/fw_bcm40183b2*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/AP6330/Wi-Fi/nvram_ap6330.txt $INSTALL/lib/firmware/brcm/ap6330_nvram.txt
+  cp -P bcm_ampak/config/AP6330/BT/bcm40183b2.hcd $INSTALL/lib/firmware/brcm
+  # AP6335
   cp -PR bcm_ampak/config/6335/fw_bcm4339a0_*.bin $INSTALL/lib/firmware/brcm
-  cp -P bcm_ampak/config/6335/nvram.txt $INSTALL/lib/firmware/brcm/nvram_ap6335.txt
+  cp -P bcm_ampak/config/6335/nvram.txt $INSTALL/lib/firmware/brcm/ap6335_nvram.txt
   cp -P bcm_ampak/config/6335/BT/bcm4335c0.hcd $INSTALL/lib/firmware/brcm
+  # AP6354
+  cp -PR bcm_ampak/config/4354/fw_bcm4354a1_*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/4354/nvram_ap6354.txt $INSTALL/lib/firmware/brcm/ap6354_nvram.txt
+  cp -P bcm_ampak/config/4354/bcm4354a1.hcd $INSTALL/lib/firmware/brcm
+  # AP6356
+  cp -PR bcm_ampak/config/4356/fw_bcm4356a2_*.bin $INSTALL/lib/firmware/brcm
+  cp -P bcm_ampak/config/4356/nvram_ap6356.txt $INSTALL/lib/firmware/brcm/ap6356_nvram.txt
+  cp -P bcm_ampak/config/4356/bcm4356a2.hcd $INSTALL/lib/firmware/brcm
 }
