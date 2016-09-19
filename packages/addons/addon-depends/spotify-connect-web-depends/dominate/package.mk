3754a67c449b9baa5dd46f7127b999b9995abef3
awiouy <awiouy@gmail.com>
2016-09-20 00:43:57 +0200

    dominate: initial package

---
+++ packages/addons/addon-depends/spotify-connect-web-depends/dominate/package.mk
@@ -0,0 +1,43 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+PKG_NAME="dominate"
+PKG_VERSION="2.2.1"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/Knio/dominate/"
+PKG_URL="https://github.com/Knio/dominate/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host"
+PKG_LONGDESC="Dominate is a Python library for creating and manipulating HTML documents using an elegant DOM API"
+
+pre_make_target() {
+  strip_lto
+  export LDSHARED="$CC -shared"
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/lib/python*/site-packages/*/tests
+}
