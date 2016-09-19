629e9eebe038dee3b0f50939ec7aae827967fd00
awiouy <awiouy@gmail.com>
2016-09-20 00:43:41 +0200

    cython: initial package

---
+++ packages/addons/addon-depends/spotify-connect-web-depends/cython/package.mk
@@ -0,0 +1,44 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+PKG_NAME="cython"
+PKG_VERSION="0.24.1"
+PKG_LICENSE="ASL"
+PKG_SITE="http://cython.org/"
+PKG_URL="https://pypi.python.org/packages/c6/fe/97319581905de40f1be7015a0ea1bd336a756f6249914b148a17eefa75dc/Cython-0.24.1.tar.gz"
+PKG_SOURCE_DIR="Cython-$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain Python distutilscross:host pycparser"
+PKG_LONGDESC="The Cython compiler for writing C extensions for the Python language"
+
+pre_make_target() {
+  strip_lto
+  export LDSHARED="$CC -shared"
+  export PYTHONXCPREFIX="$SYSROOT_PREFIX/usr"
+}
+
+make_target() {
+  python setup.py build --cross-compile
+}
+
+makeinstall_target() {
+  python setup.py install --root=$INSTALL --prefix=/usr
+}
+
+post_makeinstall_target() {
+  find $INSTALL/usr/lib -name "*.py" -exec rm -rf "{}" ";"
+  rm -rf $INSTALL/usr/lib/python*/site-packages/*/tests
+}
