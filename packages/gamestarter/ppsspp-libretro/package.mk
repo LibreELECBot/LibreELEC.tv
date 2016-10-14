c83fa1db9e85f35bbcabd37e474f10e0a3d6c3ca
Bite Your Idols! <bite-your-idols@users.noreply.github.com>
2016-10-14 17:33:18 +0200

    commit1bb9da

--- packages/gamestarter/ppsspp-libretro/package.mk
+++ packages/gamestarter/ppsspp-libretro/package.mk
@@ -16,68 +16,47 @@
 #  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="libretro-ppsspp"
-# PKG_VERSION="2ddea9a" -> OUTDATED VERSION
-PKG_VERSION="e1bb9da" 
+PKG_NAME="ppsspp-libretro"
+PKG_VERSION="e1bb9da"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-ppsspp"
-# PKG_URL="$LAKKA_MIRROR/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_URL="https://github.com/libretro/libretro-ppsspp/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="libretro"
-PKG_SHORTDESC="Libretro port of PPSSPP"
+PKG_SHORTDESC="Non-shallow fork of PPSSPP for libretro exclusively."
 PKG_LONGDESC="A fast and portable PSP emulator"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-# post_unpack() {
-#   mv $BUILD/libretro-ppsspp-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
-# git clone --depth 1 https://github.com/libretro/ppsspp-ffmpeg.git $BUILD/$PKG_NAME-$PKG_VERSION/ffmpeg
-#  git clone --depth 1 https://github.com/libretro/ppsspp-native.git $BUILD/$PKG_NAME-$PKG_VERSION/native
-#  git clone --depth 1 https://github.com/Kingcom/armips.git $BUILD/$PKG_NAME-$PKG_VERSION/ext/armips
-#}
+post_unpack() {
+  mv $BUILD/libretro-ppsspp-$PKG_VERSION* $BUILD/$PKG_NAME-$PKG_VERSION
+  git clone --depth 1 https://github.com/libretro/ppsspp-ffmpeg.git $BUILD/$PKG_NAME-$PKG_VERSION/ffmpeg
+  git clone --depth 1 https://github.com/libretro/ppsspp-native.git $BUILD/$PKG_NAME-$PKG_VERSION/native
+  git clone --depth 1 https://github.com/Kingcom/armips.git $BUILD/$PKG_NAME-$PKG_VERSION/ext/armips
+}
 
-#pre_configure_target() {
-#  strip_lto
-#}
-
-#make_target() {
-#  cd $ROOT/$PKG_BUILD/libretro
-#  if [ "$OPENGLES" == "gpu-viv-bin-mx6q" ]; then
-#    CFLAGS="$CFLAGS -DLINUX -DEGL_API_FB"
-#    CXXFLAGS="$CXXFLAGS -DLINUX -DEGL_API_FB"
-#  fi
-#  if [ "$OPENGLES" == "bcm2835-driver" ]; then
-#    CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads"
-#    CXXFLAGS="$CXXFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads"
-#  fi
-#  if [ "$ARCH" == "arm" ]; then
-#    SYSROOT_PREFIX=$SYSROOT_PREFIX make platform=imx6
-#  else
-#    make
-#  fi
-#}
-
-# makeinstall_target() {
-#  mkdir -p $INSTALL/usr/lib/libretro
-#  cp ../libretro/ppsspp_libretro.so $INSTALL/usr/lib/libretro/
-#}
-
-# make_target() {
- # make platform=rpi2 CC=$CC CXX=$CXX -j3
-#}
+pre_configure_target() {
+  strip_lto
+}
 
 make_target() {
-  make platform=rpi2 CC=$CC CXX=$CXX
+  cd $ROOT/$PKG_BUILD/libretro
+  if [ "$OPENGLES" == "gpu-viv-bin-mx6q" ]; then
+    CFLAGS="$CFLAGS -DLINUX -DEGL_API_FB"
+    CXXFLAGS="$CXXFLAGS -DLINUX -DEGL_API_FB"
+  fi
+  if [ "$ARCH" == "arm" ]; then
+    SYSROOT_PREFIX=$SYSROOT_PREFIX make platform=imx6
+  else
+    make
+  fi
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp uae4arm_libretro.so $INSTALL/usr/lib/libretro/
+  cp ../libretro/ppsspp_libretro.so $INSTALL/usr/lib/libretro/
 }
-
-
