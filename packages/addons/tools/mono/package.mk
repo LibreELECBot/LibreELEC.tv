0d1d759d2b8ada6e657159ca4d7169c11fbfc5fc
awiouy <awiouy@gmail.com>
2016-10-14 09:16:39 +0200

    mono: update to 4.6.2.7

--- packages/addons/tools/mono/package.mk
+++ packages/addons/tools/mono/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="mono"
-PKG_VERSION="4.2.1.102"
-PKG_REV="101"
+PKG_VERSION="4.6.2.7"
+PKG_REV="102"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="http://www.mono-project.com"
