4eb24ad5470ad32fc54989c689f7d67717f1c2c7
bite-your-idols <josu@onclick.es>
2016-11-18 11:15:09 +0100

    compiling tests

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -23,13 +23,9 @@ PKG_ARCH="any"
 PKG_LICENSE="various"
 PKG_SITE="http://www.libreelec.tv"
 PKG_URL=""
-<<<<<<< HEAD
-# PKG_DEPENDS_TARGET="toolchain $PROJECT"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_PRIORITY="optional"
-=======
-PKG_DEPENDS_TARGET="toolchain $PROJECT"
->>>>>>> upstream/master
+#PKG_DEPENDS_TARGET="toolchain $PROJECT"
+#PKG_PRIORITY="optional"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="OEM: Metapackage for various OEM packages"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
@@ -37,21 +33,140 @@ PKG_LONGDESC="OEM: Metapackage for various OEM packages"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-OEM_GAMESTARTER=" \
-  retroarch \
-  uae4arm \
-  ppsspp-libretro \
-  uae4arm-libretro"
+OEM_EMU_COMMON=" \
+  	glupen64-libretro \
+  	mame2003-libretro \
+  	emulationstation"
 
+OEM_EMU_GENERIC=" \
+	mame2010-libretro"
 
-if [ ! "$OEM_GAMESTARTER" = "no" ]; then
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_GAMESTARTER"
-  # case $PROJECT in
-    # Generic)
-    #  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_GENERIC"
-    #  ;;
-  #  RPi*)
-  #    PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_RPI"
-  #    ;;
-  #esac
+OEM_EMU_RPI=" \
+	desmume-libretro \
+	yabause-libretro \
+	uae4arm-libretro \
+  	retroarch"
+
+# Errors compiling:
+# Generic 7: uae4arm-libretro, retroarch (vulkan)
+# RPI2 LE7: mame2010-libretro, ppsspp-libretro, reicast-libretro, uae4arm (SDL)
+# RPI2 LE8: mame2010-libretro, ppsspp-libretro, reicast-libretro, uae4arm (SDL)
+
+if [ ! "$OEM_EMU" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_COMMON"
+  case $PROJECT in
+    Generic)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_GENERIC"
+      ;;
+    RPi*)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_RPI"
+      ;;
+  esac
 fi
+
+
+
+
+# ESCALADE OEM PACKAGE
+# PKG_NAME="oem"
+# PKG_VERSION=""
+# PKG_REV="1"
+# PKG_ARCH="any"
+# PKG_LICENSE="various"
+# PKG_SITE="http://www.libreelec.tv"
+# PKG_URL=""
+# PKG_DEPENDS_TARGET="toolchain"
+# PKG_SECTION="virtual"
+# PKG_SHORTDESC="OEM: Metapackage for various OEM packages"
+# PKG_LONGDESC="OEM: Metapackage for various OEM packages"
+
+# PKG_IS_ADDON="no"
+# PKG_AUTORECONF="no"
+
+# OEM_APPS_COMMON=" \
+#   addons-to-image \
+#   docker-system \
+#   dstat \
+#   htop \
+#   nmon \
+#   NzbDrone \
+#   plexmediaserver-script \
+#   rsync \
+#   sdl-jstest \
+#   SABnzbd \
+#   tcpdump \
+#   transmission \
+#   ttf-tlwg \
+#   unfs3 \
+#   vim"
+
+# OEM_APPS_GENERIC=" \
+#   google-chrome \
+#   spotify \
+#   tigervnc-system \
+#   qemu"
+
+# OEM_EMU_COMMON=" \
+#   beetle-wswan-libretro \
+#   dosbox-sdl \
+#   ds4drv \
+#   emulationstation \
+#   fbalpha-libretro \
+#   fuse-libretro \
+#   genesis-plus-gx-libretro \
+#   emulationstation \
+#   mame2003-libretro \
+#   mgba-libretro \
+#   mupen64plus-libretro \
+#   glupen64-libretro \
+#   nxengine-libretro \
+#   quicknes-libretro \
+#   prboom-libretro \
+#   residualvm \
+#   retroarch \
+#   scraper \
+#   scummvm-libretro \
+#   snes9x2010-libretro \
+#   tyrquake-libretro \
+#   vice"
+
+# OEM_EMU_GENERIC=" \
+#   beetle-psx-libretro \
+#   beetle-saturn-libretro \
+#   craft-libretro \
+#   desmume-libretro \
+#   dolphin \
+#   fs-uae \
+#   ppsspp-libretro \
+#   reicast-libretro \
+#   unclutter"
+
+# OEM_EMU_RPI=" \
+#   gpsp \
+#   uae4arm \
+#   pcsx_rearmed-libretro \
+#   yabause-libretro"
+
+# if [ ! "$OEM_APPS" = "no" ]; then
+#   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_COMMON"
+#   case $PROJECT in
+#     Generic)
+#       PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_GENERIC"
+#       ;;
+#     RPi*)
+#       PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_RPI"
+#       ;;
+#   esac
+# fi
+
+# if [ ! "$OEM_EMU" = "no" ]; then
+#   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_COMMON"
+#   case $PROJECT in
+#     Generic)
+#       PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_GENERIC"
+#       ;;
+#     RPi*)
+#       PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_RPI"
+#       ;;
+#   esac
+# fi
