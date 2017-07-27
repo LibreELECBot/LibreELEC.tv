3a447dde132bd2a4432813849d46c32247cc1cb5
SuperDuperHacker <nagakamira@gmail.com>
2017-07-27 15:43:25 +0300

    upgrade to linux 4.12.3

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -63,7 +63,7 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   *)
-    PKG_VERSION="4.12"
+    PKG_VERSION="4.12.3"
     PKG_SHA256="a45c3becd4d08ce411c14628a949d08e2433d8cdeca92036c7013980e93858ab"
     PKG_URL="http://www.kernel.org/pub/linux/kernel/v4.x/$PKG_NAME-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
