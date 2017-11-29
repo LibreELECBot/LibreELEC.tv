0fda893f2b20ba68c454189bea8a05cb991eaaa6
Adam Green <greena88@gmail.com>
2017-11-29 01:38:21 +0000

    media_build: add support for S905/S912

--- packages/linux-drivers/media_build/package.mk
+++ packages/linux-drivers/media_build/package.mk
@@ -31,6 +31,10 @@ PKG_SHORTDESC="DVB drivers that replace the version shipped with the kernel"
 PKG_LONGDESC="DVB drivers that replace the version shipped with the kernel"
 PKG_IS_KERNEL_PKG="yes"
 
+if [ "$PROJECT" = "S905" ] || [ "$PROJECT" = "S912" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET dvb_tv-aml"
+fi
+
 pre_make_target() {
   export KERNEL_VER=$(get_module_dir)
   export LDFLAGS=""
@@ -44,9 +48,35 @@ make_target() {
     if [ -f $PKG_DIR/config/generic.config ]; then
       cp $PKG_DIR/config/generic.config v4l/.config
     fi
-  else
+  elif [ "$PROJECT" != "S905" ] && [ "$PROJECT" != "S912" ]; then
     if [ -f $PKG_DIR/config/usb.config ]; then
       cp $PKG_DIR/config/usb.config v4l/.config
+    fi
+  fi
+
+  if [ "$PROJECT" = "S905" ] || [ "$PROJECT" = "S912" ]; then
+    # Amlogic AMLVIDEO driver
+    if [ -e "$(kernel_path)/drivers/amlogic/video_dev" ]; then
+      # Copy, patch and enable amlvideodri module
+      cp -a "$(kernel_path)/drivers/amlogic/video_dev" "linux/drivers/media/"
+      sed -i 's,common/,,g; s,"trace/,",g' $(find linux/drivers/media/video_dev/ -type f)
+      sed -i 's,\$(CONFIG_V4L_AMLOGIC_VIDEO),m,g' "linux/drivers/media/video_dev/Makefile"
+      echo "obj-y += video_dev/" >> "linux/drivers/media/Makefile"
+
+      # Copy and enable videobuf-res module
+      cp -a "$(kernel_path)/drivers/media/v4l2-core/videobuf-res.c" "linux/drivers/media/v4l2-core/"
+      cp -a "$(kernel_path)/include/media/videobuf-res.h" "linux/include/media/"
+      echo "obj-m += videobuf-res.o" >> "linux/drivers/media/v4l2-core/Makefile"
+    fi
+
+    # Amlogic DVB driver
+    DVB_TV_AML_DIR="$(get_build_dir dvb_tv-aml)"
+    if [ -d "$DVB_TV_AML_DIR" ]; then
+      cp -a "$DVB_TV_AML_DIR" "linux/drivers/media/dvb_tv"
+      echo "obj-y += dvb_tv/" >> "linux/drivers/media/Makefile"
+    fi
+    if [ "$PROJECT" = "S905" ]; then
+      echo "obj-y += amlogic/dvb_tv/" >> "linux/drivers/media/Makefile"
     fi
   fi
 
