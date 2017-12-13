7e3a648f168ed94040147dec5074ac5622c2885e
thoradia <thoradia.quack@gmail.com>
2017-12-13 22:15:07 +0100

    iso8601: initial package

---
+++ packages/python/system/iso8601/package.mk
@@ -0,0 +1,28 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="iso8601"
+PKG_VERSION="0.1.12"
+PKG_SHA256="75ae5e019abc14e52b4a57c1aab2a436f8ec6942c8be7a4934d2e4fcbfbf3bae"
+PKG_LICENSE="MIT"
+PKG_SITE="https://bitbucket.org/micktwomey/pyiso8601"
+PKG_URL="https://bitbucket.org/micktwomey/pyiso8601/get/$PKG_VERSION.tar.bz2"
+PKG_SOURCE_DIR="micktwomey-pyiso8601-*"
+PKG_LONGDESC="Simple module to parse ISO 8601 dates"
+
+PKG_TOOLCHAIN="python"
