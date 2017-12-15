2851342b51cbf397becc1286cb5b5518cbbf0c7a
thoradia <thoradia.quack@gmail.com>
2017-12-15 13:50:50 +0100

    six: initiial package

---
+++ packages/python/system/six/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="six"
+PKG_VERSION="1.11.0"
+PKG_SHA256="927dc6fcfccd4e32e1ce161a20bf8cda39d8c9d5f7a845774486907178f69bd4"
+PKG_LICENSE="MIT"
+PKG_SITE="http://pypi.python.org/pypi/six"
+PKG_URL="https://github.com/benjaminp/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Python 2 and 3 compatibility utilities"
+
+PKG_TOOLCHAIN="python"
