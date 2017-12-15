77499990a9cac21ca7f5fadf664a333003be787f
thoradia <thoradia.quack@gmail.com>
2017-12-15 17:35:05 +0100

    asn1crypto: initial package

---
+++ packages/python/security/asn1crypto/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="asn1crypto"
+PKG_VERSION="0.23.0"
+PKG_SHA256="884b5ba3d9b442c1729f67981d3ddeb85a1d7dbd35199000a0bc8d077548352d"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/wbond/asn1crypto"
+PKG_URL="https://github.com/wbond/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Python ASN.1 library with a focus on performance and a pythonic API"
+
+PKG_TOOLCHAIN="python"
