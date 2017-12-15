f184405dd2ebb60db984c3c5c3593fb1fa92045b
thoradia <thoradia.quack@gmail.com>
2017-12-15 17:51:57 +0100

    idna: initial package

---
+++ packages/python/system/idna/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="idna"
+PKG_VERSION="2.6"
+PKG_SHA256="53c722c4b7908dfdf2e5db2b79982f1084494db7b34fd31ff6a296e9fddfceaa"
+PKG_LICENSE="BSD"
+PKG_SITE="https://github.com/kjd/idna"
+PKG_URL="https://github.com/kjd/$PKG_NAME/archive/v$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Internationalized Domain Names for Python (IDNA 2008 and UTS #46)"
+
+PKG_TOOLCHAIN="python"
