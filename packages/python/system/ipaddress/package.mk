fa7106264ca39539993abd7ad939ae26ab858d1b
thoradia <thoradia.quack@gmail.com>
2017-12-15 17:56:08 +0100

    ipaddress: initial package

---
+++ packages/python/system/ipaddress/package.mk
@@ -0,0 +1,27 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2017-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="ipaddress"
+PKG_VERSION="1.0.18"
+PKG_SHA256="8ddc806938a3ee874ca0a738247c68ba255f734dd3d930e33264f3408952bcd5"
+PKG_LICENSE="PSF"
+PKG_SITE="https://github.com/phihag/ipaddress"
+PKG_URL="https://github.com/phihag/$PKG_NAME/archive/v$PKG_VERSION.tar.gz"
+PKG_LONGDESC="Python 3.3's ipaddress for older Python versions"
+
+PKG_TOOLCHAIN="python"
