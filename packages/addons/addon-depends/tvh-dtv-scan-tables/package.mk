f7133598b2d772150cf7d1db4eaf0eca27899901
Matthias Reichl <hias@horus.com>
2018-01-15 01:05:19 +0100

    tvh-dtv-scan-tables: new dependency package for Tvheadend
    Signed-off-by: Matthias Reichl <hias@horus.com>

---
+++ packages/addons/addon-depends/tvh-dtv-scan-tables/package.mk
@@ -0,0 +1,30 @@
+################################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
+#      Copyright (C) 2018-present Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="tvh-dtv-scan-tables"
+PKG_VERSION="7af1ed5"
+PKG_SHA256="000f1301e3b67c14ceae535ece4e5af6fe7c6f7436bac8e09deffb92f8c6fd3b"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.tvheadend.org"
+PKG_URL="https://github.com/tvheadend/dtv-scan-tables/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="dtv-scan-tables-${PKG_VERSION}*"
+PKG_SHORTDESC="Tvheadend DTV scan tables"
+PKG_LONGDESC="Tvheadend DTV scan tables"
+PKG_TOOLCHAIN="manual"
+
