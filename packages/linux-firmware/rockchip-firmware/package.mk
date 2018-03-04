50454b751a06bb478f9c00d89ea67faa346a4be2
Jonas Karlman <jonas@kwiboo.se>
2018-03-04 15:57:07 +0100

    fixup! RK3399: new device

--- packages/linux-firmware/rockchip-firmware/package.mk
+++ packages/linux-firmware/rockchip-firmware/package.mk
@@ -29,7 +29,14 @@ PKG_LONGDESC="rockchip firmware"
 PKG_TOOLCHAIN="manual"
 
 makeinstall_target() {
+  mkdir -p $INSTALL/usr/bin
+    cp -v $(get_build_dir rkbin)/firmware/bin/rtk_hciattach $INSTALL/usr/bin
+
+  mkdir -p $INSTALL/$(get_full_firmware_dir)/rtlbt
+    cp -v $(get_build_dir rkbin)/firmware/bluetooth/rtl8723b_* $INSTALL/$(get_full_firmware_dir)/rtlbt
+
   mkdir -p $INSTALL/$(get_full_firmware_dir)/brcm
+    cp -v $(get_build_dir rkbin)/firmware/bluetooth/BCM4354A2.hcd $INSTALL/$(get_full_firmware_dir)/brcm
     cp -v $(get_build_dir rkbin)/firmware/wifi/fw_bcm4356a2_ag.bin $INSTALL/$(get_full_firmware_dir)/brcm
     cp -v $(get_build_dir rkbin)/firmware/wifi/nvram_ap6356.txt $INSTALL/$(get_full_firmware_dir)/brcm
 }
