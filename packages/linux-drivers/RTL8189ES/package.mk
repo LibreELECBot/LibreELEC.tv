6ad909d622ab53ce5951cb0c1b04d19cd5ccac71
vovanys <admin@vovanys.com>
2019-01-29 13:34:05 +0300

    Add Wifi driver for OrangePi Plus

---
+++ packages/linux-drivers/RTL8189ES/package.mk
@@ -0,0 +1,28 @@
+PKG_NAME="RTL8189ES"
+PKG_VERSION="rtl8189fs"
+PKG_SHA256="6d41578af502ed665e3d84cee3de14c26be85e396425c2e4e04e3814070f931d"
+PKG_LICENSE="GPL"
+# realtek: PKG_SITE="http://www.realtek.com.tw/downloads/downloadsView.aspx?Langid=1&PFid=48&Level=5&Conn=4&ProdID=274&DownTypeID=3&GetDown=false&Downloads=true"
+PKG_SITE="https://github.com/jwrdegoede/rtl8189ES_linux"
+PKG_URL="https://github.com/jwrdegoede/rtl8189ES_linux/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux"
+PKG_NEED_UNPACK="$LINUX_DEPENDS"
+PKG_LONGDESC="Realtek 8189E SDIO WiFi Linux driver"
+PKG_IS_KERNEL_PKG="yes"
+
+pre_make_target() {
+  unset LDFLAGS
+}
+
+make_target() {
+  make V=1 \
+       ARCH=$TARGET_KERNEL_ARCH \
+       KSRC=$(kernel_path) \
+       CROSS_COMPILE=$TARGET_KERNEL_PREFIX \
+       CONFIG_POWER_SAVING=n
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/$(get_full_module_dir)/$PKG_NAME
+    cp *.ko $INSTALL/$(get_full_module_dir)/$PKG_NAME
+}
