dcb5db7240346f453c71448ddc8f4d11daf90a58
MilhouseVH <milhouseVH.github@nmacleod.com>
2020-01-06 20:26:55 +0000

    linux (RPi/RPi2/RPi4): update to linux-50649cd (5.5-rc5)

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -23,8 +23,8 @@ case "$LINUX" in
     PKG_PATCH_DIRS="amlogic"
     ;;
   raspberrypi)
-    PKG_VERSION="2d26b74c133847286842ff1a2c8f5d4c1837bc30" # 5.4.16
-    PKG_SHA256="e5dd94565a1808b0877b2722c91370695756df7838c8a13e20a497f0d7169b34"
+    PKG_VERSION="50649cdc35426f2bfeb08902bcf224c2b3bb9867" # 5.5-rc5
+    PKG_SHA256="d67ea629a040df74f3b19a3e9dfb80f6b35f7bb8388257a7145fbb90eaed4ed0"
     PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
     PKG_SOURCE_NAME="linux-$LINUX-$PKG_VERSION.tar.gz"
     ;;
