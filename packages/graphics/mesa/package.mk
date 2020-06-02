4ad5ca54819dbbe287c71fa79ab52d28d34e0873
nouman8 <nouman8@hotmail.com>
2020-06-02 12:01:17 +0500

    mesa: update to mesa-20.1.0

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="20.0.6"
-PKG_SHA256="49eb16f041773b6eaff5da8bf355a285b17b500d7631dee98712061f9e879da4"
+PKG_VERSION="20.1.0"
+PKG_SHA256="36fe6db41f2971ac073eabd286697733609dd543dbb3784319c30d5f64eac3f0"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://github.com/mesa3d/mesa/archive/mesa-${PKG_VERSION}.tar.gz"
