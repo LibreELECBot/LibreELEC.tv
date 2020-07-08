bb4b63d75fef659a9942f0ab1916f4a464fceedb
Matthias Reichl <hias@horus.com>
2020-07-08 14:21:25 +0200

    fixup ffmpeg merge script, git pull instead of fetch+merge
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- tools/ffmpeg/merge-feature-branches.sh
+++ tools/ffmpeg/merge-feature-branches.sh
@@ -2,16 +2,15 @@
 
 # useage: merge-feature-branches [--rpi]
 
-merge() {
-  git fetch $1 $2
-  git merge --no-ff --log FETCH_HEAD
+pull() {
+  git pull --no-ff --log "$@"
 }
 
-merge https://github.com/xbmc/FFmpeg.git 4.3-Matrix-Alpha1
-merge https://github.com/lrusak/FFmpeg.git v4l2-drmprime-v5
-merge https://github.com/Kwiboo/FFmpeg.git v4l2-request-hwaccel-4.3
-merge https://github.com/LibreELEC/FFmpeg.git 4.3-libreelec-misc
+pull https://github.com/xbmc/FFmpeg.git 4.3-Matrix-Alpha1
+pull https://github.com/lrusak/FFmpeg.git v4l2-drmprime-v5
+pull https://github.com/Kwiboo/FFmpeg.git v4l2-request-hwaccel-4.3
+pull https://github.com/LibreELEC/FFmpeg.git 4.3-libreelec-misc
 
 if [ "$1" = "--rpi" ]; then
-  merge https://github.com/jc-kynesim/rpi-ffmpeg.git test/4.3/kodi_main
+  pull https://github.com/jc-kynesim/rpi-ffmpeg.git test/4.3/kodi_main
 fi
