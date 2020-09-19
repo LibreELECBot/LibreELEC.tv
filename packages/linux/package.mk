7e25e2daaa4747d1b419e480c57f1efd5bb3393c
chewitt <christianshewitt@gmail.com>
2020-09-19 15:05:01 +0000

    linux: bump Amlogic to Linux 5.15.y kernel

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -16,8 +16,8 @@ PKG_PATCH_DIRS="${LINUX}"
 
 case "${LINUX}" in
   amlogic)
-    PKG_VERSION="6cc049b8e0d05e1519d71afcf2d40d3aa5a48366" # 5.11.10
-    PKG_SHA256="d5f4a33af53ef0b22049366b2ae2c30a9bf5741dce7d1d2ed6e499c1d9d31c20"
+    PKG_VERSION="519d81956ee277b4419c723adfb154603c2565ba" # 5.15-rc6
+    PKG_SHA256="267b05b50d5024aecc5ffa13e6afd760ec600118cfa58f7c7175e4d1fbde03ec"
     PKG_URL="https://github.com/torvalds/linux/archive/${PKG_VERSION}.tar.gz"
     PKG_SOURCE_NAME="linux-${LINUX}-${PKG_VERSION}.tar.gz"
     ;;
