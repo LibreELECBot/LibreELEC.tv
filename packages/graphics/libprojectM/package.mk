770bbb04206d55de53348cbc8bd2b016f329e0b5
Benjamin Valentin <benjamin.valentin@ml-pa.com>
2021-12-11 17:18:31 +0100

    libprojectM: update to 3.1.12

--- packages/graphics/libprojectM/package.mk
+++ packages/graphics/libprojectM/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libprojectM"
-PKG_VERSION="3.1.7"
-PKG_SHA256="968551c5d6292179838bf5d3ab85f5bd785c6fec7c1de42c0cf5ec3dbf4b04f9"
+PKG_VERSION="3.1.12"
+PKG_SHA256="b6b99dde5c8f0822ae362606a0429628ee478f4ec943a156723841b742954707"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/projectM-visualizer/projectm"
-PKG_URL="https://github.com/projectM-visualizer/projectm/archive/v${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/projectM-visualizer/projectm/releases/download/v${PKG_VERSION}/projectM-${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain freetype glm ${OPENGL}"
 PKG_LONGDESC="A MilkDrop compatible opensource music visualizer."
 PKG_TOOLCHAIN="configure"
@@ -18,9 +18,5 @@ PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
                            --disable-qt \
                            --disable-pulseaudio \
                            --disable-jack \
+                           --disable-threading \
                            --enable-preset-subdirs"
-
-# workaround due broken release files, remove at next bump
-pre_configure_target() {
-  ./autogen.sh
-}
