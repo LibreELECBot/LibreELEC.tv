97574c95d28858d1cc1986dfea7a7cdb56475027
Nuive <nuive.code@gmail.com>
2022-01-18 23:02:16 +0100

    espeak-ng: fix build problems

--- packages/audio/espeak-ng/package.mk
+++ packages/audio/espeak-ng/package.mk
@@ -3,19 +3,30 @@
 
 PKG_NAME="espeak-ng"
 PKG_VERSION="1.50"
-PKG_SHA256="80ee6cd06fcd61888951ab49362b400e80dd1fac352a8b1131d90cfe8a210edb"
+PKG_SHA256="3dc31309f1674f2009cb53c466771116a183a8d71abf18d80a4e39615d61ae5e"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/espeak-ng/espeak-ng"
-PKG_URL="https://github.com/espeak-ng/espeak-ng/releases/download/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tgz"
-PKG_DEPENDS_HOST="gcc:host "
+PKG_URL="https://github.com/espeak-ng/espeak-ng/archive/refs/tags/${PKG_VERSION}.zip"
+PKG_DEPENDS_HOST="toolchain:host "
 PKG_DEPENDS_TARGET="toolchain espeak-ng:host"
 PKG_LONGDESC="eSpeak NG is an open source speech synthesizer that supports more than a hundred languages and accents"
-PKG_TOOLCHAIN="configure"
+PKG_TOOLCHAIN="autotools"
+
+post_unpack() {
+  ln -sf "${PKG_BUILD}/CHANGELOG.md" "${PKG_BUILD}/NEWS"
+  ln -sf "${PKG_BUILD}/README.md" "${PKG_BUILD}/README"
+}
 
 pre_configure() {
-  cd ..
-  make distclean
-  ./autogen.sh
+  cp -r ../docs .
+  cp -r ../src .
+}
+
+pre_configure_host() {
+  cp -r ../dictsource .
+  cp -r ../espeak-ng-data .
+  cp -r ../phsource .
+  cp -r ../vim .
 }
 
 make_host() {
