3fffb5235803f0aaabdebbdca407e9a85bf8a15b
Lukas Rusak <lorusak@gmail.com>
2022-05-25 09:52:11 -0700

    musl-obstack: add package

---
+++ packages/devel/musl-obstack/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="musl-obstack"
+PKG_VERSION="1.2.3"
+PKG_SHA256="9ffb3479b15df0170eba4480e51723c3961dbe0b461ec289744622db03a69395"
+PKG_LICENSE="GPL-2.0-or-later"
+PKG_SITE="https://github.com/void-linux/musl-obstack"
+PKG_URL="https://github.com/void-linux/musl-obstack/archive/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="gcc:host"
+PKG_LONGDESC="Implementation of obstack for musl libc"
+PKG_TOOLCHAIN="autotools"
