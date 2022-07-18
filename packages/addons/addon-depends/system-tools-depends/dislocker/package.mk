08ca98153a069a262a4ac7ae45c2ff0605bc902f
Andrea Scarpino <andrea@scarpino.dev>
2022-07-18 11:19:39 +0200

    dislocker: initial commit

---
+++ packages/addons/addon-depends/system-tools-depends/dislocker/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="dislocker"
+PKG_VERSION="0.7.3"
+PKG_SHA256="8d5275577c44f2bd87f6e05dd61971a71c0e56a9cbedf000bd38deadd8b6c1e6"
+PKG_LICENSE="GPL2"
+PKG_SITE="https://github.com/Aorimn/dislocker"
+PKG_URL="https://github.com/Aorimn/dislocker/archive/refs/tags/v${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain fuse"
+PKG_DEPENDS_CONFIG="libmbedtls"
+PKG_LONGDESC="FUSE driver to read/write Windows' BitLocker-ed volumes."
+PKG_BUILD_FLAGS="-sysroot"
+
+PKG_CMAKE_OPTS_TARGET="-DCMAKE_INSTALL_PREFIX=/usr \
+                       -DLIB_INSTALL_DIR=/usr/lib \
+                       -DCMAKE_SKIP_RPATH=ON \
+                       -DWITH_RUBY=OFF"
