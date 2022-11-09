8d9060aa609ab0caa9b29fcb0417a87761ddca52
Tomasz Świątek <57900186+devegoo@users.noreply.github.com>
2022-11-09 13:41:40 +0100

    Create package.mk

---
+++ packages/tools/expect/package.mk
@@ -0,0 +1,4 @@
+49a7da83b0bdd9f46d04a04deec19c7767bb9a323e40c4781f89caf760b92c34  expect5.45.4.tar.gz
+https://sourceforge.net/projects/expect/files/Expect/5.45.4/expect5.45.4.tar.gz
+/usr/local/src/tcl8.0		(actual version may be different)
+/usr/local/src/expect-5.24
