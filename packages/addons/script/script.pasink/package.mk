d23a91002df3dfd2654265a104d169abb49833fe
Tomasz Świątek <57900186+devegoo@users.noreply.github.com>
2022-11-09 16:38:02 +0100

    Update package.mk

--- packages/addons/script/script.pasink/package.mk
+++ packages/addons/script/script.pasink/package.mk
@@ -1,1 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="script.pasink"
+PKG_VERSION="2.1.0"
+PKG_SHA256="793676258c399427047a7d6628984358c67b1180b98df44b48647d20e8f9395b"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="ttps://github.com/Heckie75/kodi-addon-pasin"
+PKG_URL="https://github.com/Heckie75/kodi-addon-pasink.git"
+PKG_DEPENDS_TARGET="bluez expect gawk pulseaudio "
+PKG_SECTION=""
+PKG_SHORTDESC="script.pasink"
+PKG_LONGDESC="script.pasink"
+PKG_TOOLCHAIN="manual"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="dummy"
+
+rm *.zip 
+
+
 
