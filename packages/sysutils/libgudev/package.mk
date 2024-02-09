49c859c48e865e8ec11e766a41ed962cf12c7ef0
Ronald Brown <rbrown4014@yahoo.com>
2024-02-09 05:50:45 +0100

    Fix Battery info

---
+++ packages/sysutils/libgudev/package.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libgudev"
+PKG_VERSION="235"
+PKG_SHA256="36360e7629b762b0cc85ef302b88a7cea96b0156fd426274815b155c83732c59"
+PKG_LICENSE="GPL"
+PKG_SITE="https://wiki.gnome.org/Projects/libgudev"
+PKG_URL="https://github.com/GNOME/libgudev/archive/refs/tags/${PKG_VERSION}.zip"
+PKG_DEPENDS_TARGET="toolchain systemd"
