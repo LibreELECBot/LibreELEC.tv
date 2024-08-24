825a7d9e0bd8b4ab73a6f1bccb19898b88f1d712
CvH <1355173+CvH@users.noreply.github.com>
2024-08-24 15:01:54 +0200

    Update package.mk

--- packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
+++ packages/mediacenter/kodi-binary-addons/pvr.mythtv/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pvr.mythtv"
-PKG_VERSION="21.1.9-Omega"
-PKG_SHA256="d3a54798f38befeaeaf57dec7ea85e3c8846591298ced3f686ac03d62edeb0f9"
+PKG_VERSION="21.1.7-Omega"
+PKG_SHA256="1b9f941326d223d17ff1cccdd8311baa7514edc9491f22b627d076290ad36377"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
